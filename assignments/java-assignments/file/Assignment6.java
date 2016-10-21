c2f277dbd6f325c31bfc98299104123f780f9da1
Your Name <you@example.com>
2016-10-21 13:42:52 +0530

    mend
    java files

--- assignments/java-assignments/file/Assignment6.java
+++ assignments/java-assignments/file/Assignment6.java
@@ -1,43 +1,103 @@
 package com.file;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
+
 
 import com.file.Account5;
 
 public class Assignment6 {
-
-	public static void main(String[] args) {
+	static File f;
+	static RandomAccessFile raf;
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		File f = new File("assignment6.txt");
 		
-		try {
-			RandomAccessFile raf = new RandomAccessFile(f, "rw");
-			Account5 a = new Account5(1, "Abc", 10000, new Date(26, 6, 1994));
-			Account5 a1 = new Account5(2, "Def", 1000, new Date(21, 2, 1998));
-			raf.writeInt(a.getAc_no());
-			raf.writeUTF(a.getName());
-			raf.writeLong(a.getBalance());
-			raf.writeInt(a1.getAc_no());
-			raf.writeUTF(a1.getName());
-			raf.writeLong(a1.getBalance());
+		f = new File("assignment6.txt");
+		raf = new RandomAccessFile(f, "rw");
+	
+		int choice = 0;
+		int ac_count = 1;
+		while(true){
 			
-			raf.seek(0);
-			System.out.println("Raf data: ");
-			while(raf.getFilePointer() != raf.length()){
-				Account5 b = new Account5(raf.readInt(), raf.readUTF(), raf.readLong(), null);
-				if(b.getBalance()>=10000)
-				System.out.println(b);
-			}
+		System.out.println("Enter YOur choice:\n1)Create\n2)Read\n3)Update\n4)Delete:");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		choice = Integer.parseInt(br.readLine());
+		switch(choice){
+		case 1:
+			createAccount(new Account5(ac_count, "Abc", 10000, null));
+			ac_count++;
+			break;
+		case 2:
+			readAccount();
+			break;
+		case 3:
+			updateAccount(2);
+			break;
+		case 4:
+			deleteAccount(2);
+			break;
+		case 5:
 			raf.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.exit(0);
+		
 		}
+		
 	}
+		
+	
+}
+	
+	
 
+	public static void createAccount(Account5 a) throws IOException{
+		raf.writeInt(a.getAc_no());
+		raf.writeUTF(a.getName());
+		raf.writeLong(a.getBalance());
+	}
+	
+	public static void readAccount() throws IOException{
+		raf.seek(0);
+		System.out.println("Raf data: ");
+		while(raf.getFilePointer() != raf.length()){
+			Account5 b = new Account5(raf.readInt(), raf.readUTF(), raf.readLong(), null);
+			if(b.getBalance()>=10000)
+			System.out.println(b);
+		}
+		
+	}
+	
+	public static void updateAccount(int ac_no) throws IOException{
+		raf.seek(0);
+		
+		while(raf.getFilePointer() != raf.length()){
+			if(raf.readInt()==ac_no){
+				raf.writeUTF("DEF");
+				raf.writeLong(10000);
+				
+			}else{
+				raf.readUTF(); 
+				raf.readLong();
+			}
+		}
+		System.out.println("Updated ");
+	}
+	
+	public static void deleteAccount(int ac_no) throws IOException{
+		raf.seek(0);
+		
+		while(raf.getFilePointer() != raf.length()){
+			if(raf.readInt()==ac_no){
+				raf.setLength(raf.getFilePointer()-4);
+			}else{
+				raf.readUTF(); 
+				raf.readLong();
+			}
+		}
+		System.out.println("Deleted ");
+	}
+	
+	
+	
 }
